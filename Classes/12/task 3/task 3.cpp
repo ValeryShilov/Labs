95de0927f92bf49ef7a6616ca2308f412b19d407
ValeryShilov <valera.shilov.03@mail.ru>
2023-06-12 22:36:43 +0500

    12 cl

---
+++ Classes/12/task 3/task 3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <map>
+#include <D:\Labs\Lab 11 cl\task 2\Time.cpp>
+#include <D:\Labs\Lab 11 cl\task 2\Time.h>
+#include <iostream>
+#include "vector.h"
+using namespace std;
+
+int main()
+{
+    Vector<Time> container(3);
+    setlocale(LC_ALL, "Russian");
+    int n;
+    container.Print();
+    cout << endl;
+
+    Time a = container.max();
+    container.add(a);
+    container.Print();
+    cout << endl;
+
+    int b = container.min();
+    container.del(b);
+    container.Print();
+    cout << endl;
+
+    Time c = container.sred();
+    container.add_sred(c);
+    container.Print();
+    cout << endl;
+    return 0;
+}
